550aa1f27cc99d802ddc003f07ebe6761ca9daa6
Tomoya Yamaguchi <tomoya.a.yamaguchi@mizuho-bk.co.jp>
2026-01-16 12:06:42 +0900

    修正

--- handson/frontend/src/main.jsx
+++ handson/frontend/src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
+
+// デバッグ用: コンソールに値が出るか確認してください
+console.log("Domain Check:", import.meta.env.VITE_AUTH0_DOMAIN);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
